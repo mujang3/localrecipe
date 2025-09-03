6cec996f7911e6bd0a80009fca7b66b67023c6b0
carpbreadj <carpbreadj@gmail.com>
2025-09-03 11:08:50 +0900

    Initial commit

---
+++ src/main/java/org/localrecipe/LocalRecipeApplication.java
@@ -0,0 +1,13 @@
+package org.localrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalRecipeApplication.class, args);
+    }
+
+}
